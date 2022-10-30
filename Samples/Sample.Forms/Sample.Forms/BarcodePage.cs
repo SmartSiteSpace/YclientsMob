1e6b10994ef090227250b8b544bf882a720a3f07
Дмитрий Ульянов <info@smartmed.info>
2022-10-30 06:30:33 +0300

    first commit

---
+++ Samples/Sample.Forms/Sample.Forms/BarcodePage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using ZXing.Net.Mobile.Forms;
+
+namespace Sample.Forms
+{
+	public class BarcodePage : ContentPage
+	{
+		
+		public BarcodePage(string Link)
+		{
+			var stackLayout = new StackLayout
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+			};
+
+
+			var browser = new WebView 
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                AutomationId = "browser",
+            };
+
+            //var htmlSource = new HtmlWebViewSource();
+            //htmlSource.Html = @"<html><body>
+            //  <h1>Xamarin.Forms</h1>
+            //  <p>Welcome to WebView.</p>
+            //  </body></html>";
+            //browser.Source = htmlSource;
+            var url = "https://ateri.smartmed.info/" + Link;
+            browser.Source = url;
+            browser.Navigating += (s, e) =>
+            {
+                if (!e.Url.Contains(url))
+                {
+                    try
+                    {
+                        //отменяем загрузку
+                        //e.Cancel = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        var err = ex.Message;
+                    }
+                }
+            };
+            //stackLayout.Children.Add(barcode);
+            stackLayout.Children.Add(browser);
+            stackLayout.IsVisible = true;
+
+            Content = stackLayout;
+            this.Barcode = Barcode;
+        }
+
+        public string Barcode { get; }
+
+
+
+        //	barcode = new ZXingBarcodeImageView
+        //		{
+        //			HorizontalOptions = LayoutOptions.FillAndExpand,
+        //			VerticalOptions = LayoutOptions.FillAndExpand,
+        //			AutomationId = "zxingBarcodeImageView",
+        //		};
+        //barcode.BarcodeFormat = ZXing.BarcodeFormat.QR_CODE;
+        //		barcode.BarcodeOptions.Width = 300;
+        //		barcode.BarcodeOptions.Height = 300;
+        //		barcode.BarcodeOptions.Margin = 10;
+        //		barcode.BarcodeValue = "ZXing.Net.Mobile";
+
+        //		var text = new Entry
+        //		{
+        //			HorizontalOptions = LayoutOptions.FillAndExpand,
+        //			Text = "ZXing Sample"
+        //		};
+        //text.TextChanged += Text_TextChanged;
+
+
+
+        //	void Text_TextChanged(object sender, TextChangedEventArgs e)
+        //		=> barcode.BarcodeValue = e.NewTextValue;
+    }
+}
+
---
+++ Samples/Sample.Forms/Sample.Forms/MainPage.xaml.cs
@@ -0,0 +1,96 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sample.Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+
+        string url = "https://ateri.smartmed.info";
+
+        public MainPage(string result)
+        {
+            InitializeComponent();
+            //задаем ширину и высоту webview
+            webView.HeightRequest = DeviceDisplay.MainDisplayInfo.Height;
+            webView.WidthRequest = DeviceDisplay.MainDisplayInfo.Width;
+            //грузим контент
+            url = url + "/?id=" + result;
+            LoadUrl();
+        }
+
+        public void LoadUrl()
+        {
+            //открываем в webview ресурс
+            webView.Source = url;
+            //показываем webview
+            webView.IsVisible = true;
+            //скрываем кнопку buttonConnect
+            buttonConnect.IsVisible = true;
+
+        }
+
+        //нажатие кнопки
+        void ButtonConnect_Clicked(object sender, EventArgs e)
+        {
+            //грузим контент
+            LoadUrl();
+        }
+
+        //загрузка url через webview
+        void WebView_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+        
+
+        }
+
+        public void webView_Navigating_1(object sender, WebNavigatingEventArgs e)
+        {
+            //если видим, что загружаемый адрес не содержит нашего исходного адреса (происходит загрузка стороннего ресурса)            
+            if (!e.Url.Contains(url))
+            {
+                try
+                {
+                    //отменяем загрузку
+                    e.Cancel = true;
+                    //открываем этот ресурс через обычный браузер  
+                    Browser.OpenAsync(e.Url, BrowserLaunchMode.SystemPreferred);
+                }
+                catch (Exception ex)
+                {
+                    var err = ex.Message;
+                }
+            }
+            //отслеживаем наличие соединение с сетью Интернет с помощью Xam.Plugin.Connectivity
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                //если нет соединения, а webview при этом видимый, то отменяем загрузку, прячем webview и показываем кнопку buttonConnect
+                if (webView.IsVisible == true)
+                {
+                    e.Cancel = true;
+                    webView.IsVisible = false;
+                    buttonConnect.IsVisible = true;
+                }
+            }
+            else
+            {
+                //если соединение есть, а webview при этом скрыт, то прячем кнопку buttonConnect и показываем webview 
+                if (webView.IsVisible == false)
+                {
+                    webView.IsVisible = true;
+                    buttonConnect.IsVisible = false;
+                }
+            }
+
+        }
+    }
+}
